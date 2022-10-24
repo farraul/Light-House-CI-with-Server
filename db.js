3ce653741aad054f2b3d05ca537e0b2f3130c00a
raul far <raulfarllobell@gmail.com>
2022-10-24 16:12:13 +0200

    changes t lighthouse

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 module.exports  = [
-    {id:1, title: "TTrao Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busan"},
+    {id:1, title: "TTrao Busarain to Brain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busarain to Busan"},
     {id:2, title: "Shaun of the dead"},
     {id:3, title: "Zombieland"},
  
